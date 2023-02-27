e40436e809ca80233c2b94c8f7b09259d5de53fb
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-02-27 17:09:25 +0000

    added prisons home page

--- server/app.ts
+++ server/app.ts
@@ -18,6 +18,8 @@ import setUpWebSecurity from './middleware/setUpWebSecurity'
 import setUpWebSession from './middleware/setUpWebSession'
 
 import routes from './routes'
+import prisonsRoutes from './routes/prisons'
+
 import type { Services } from './services'
 
 export default function createApp(services: Services): express.Application {
@@ -40,6 +42,7 @@ export default function createApp(services: Services): express.Application {
   app.use(setUpCurrentUser(services))
 
   app.use(routes(services))
+  app.use(prisonsRoutes(services))
 
   app.use((req, res, next) => next(createError(404, 'Not found')))
   app.use(errorHandler(process.env.NODE_ENV === 'production'))
