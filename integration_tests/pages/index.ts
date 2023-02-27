e2a67fe6f68d0f999e89b32b0e99d40d9a8d72cd
aled.wyn.evans <aled.wynevans@digital.justice.gov.uk>
2023-02-27 12:51:48 +0000

    Found other issues with integration tests

--- integration_tests/pages/index.ts
+++ integration_tests/pages/index.ts
@@ -2,7 +2,7 @@ import Page, { PageElement } from './page'
 
 export default class IndexPage extends Page {
   constructor() {
-    super('VSIP internal admin')
+    super('VSIP Internal Admin')
   }
 
   headerUserName = (): PageElement => cy.get('[data-qa=header-user-name]')
--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -10,7 +10,7 @@ export default function nunjucksSetup(app: express.Express, path: pathModule.Pla
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'Hmpps Vsip Internal Admin Ui'
+  app.locals.applicationName = 'VSIP Internal Admin'
 
   // Cachebusting version string
   if (production) {
