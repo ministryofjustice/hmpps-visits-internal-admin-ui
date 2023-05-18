70cc2aa105e908c89153cabebd7b1fbb6b530622
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-05-18 21:14:16 +0100

    Fix version not appearing in application insights
    From https://github.com/ministryofjustice/hmpps-template-typescript/pull/190

--- server/applicationVersion.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-
-const packageData = JSON.parse(fs.readFileSync('./package.json').toString())
-const { buildNumber, gitRef } = fs.existsSync('./build-info.json')
-  ? JSON.parse(fs.readFileSync('./build-info.json').toString())
-  : { buildNumber: packageData.version, gitRef: 'xxxxxxxx' }
-
-export default { buildNumber, packageData, shortHash: gitRef.substring(0, 8) }
