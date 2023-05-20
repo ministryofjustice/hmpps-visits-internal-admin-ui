9fadfcee67721022532eb5ae4cba00ac5ed3f7a0
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-05-20 16:02:37 +0100

    Show list of supported prison IDs and names

--- server/routes/prisons/prisons.ts
+++ server/routes/prisons/prisons.ts
@@ -3,13 +3,19 @@ import { type RequestHandler, Router } from 'express'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import type { Services } from '../../services'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function routes(services: Services): Router {
+export default function routes({ supportedPrisonsService }: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', (req, res) => {
     res.render('pages/prisons/index')
   })
+
+  get('/supported', async (req, res) => {
+    const supportedPrisons = await supportedPrisonsService.getSupportedPrisons(res.locals.user.username)
+
+    res.render('pages/prisons/supported', { supportedPrisons })
+  })
+
   return router
 }
