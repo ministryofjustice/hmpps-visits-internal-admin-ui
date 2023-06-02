03759843edcbf3d571b195197c23cbd04975d2c5
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2023-06-02 16:47:20 +0100

    Added success/error message to de/activate page(forced)

--- server/routes/prisons/prisons.ts
+++ server/routes/prisons/prisons.ts
@@ -1,5 +1,6 @@
 import { type RequestHandler, Router } from 'express'
 
+import { validationResult } from 'express-validator'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
 import type { Services } from '../../services'
 
@@ -16,22 +17,29 @@ export default function routes({ prisonService }: Services): Router {
     const prisons = await prisonService.getAllPrisons(res.locals.user.username)
     const prisonNames = await prisonService.getPrisonNames(res.locals.user.username)
 
-    res.render('pages/prisons/prisons', { prisons, prisonNames })
+    res.render('pages/prisons/prisons', { prisons, prisonNames, errors: req.flash('errors') })
   })
 
   get('/:prisonId([A-Z]{3})/edit', async (req, res) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { prisonId } = req.params
+    const confirmedChange = req.query?.confirmedChange
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      req.flash('errors', errors.array() as [])
+      return res.redirect('/change-establishment')
+    }
 
     const prisons = await prisonService.getAllPrisons(res.locals.user.username)
     const prisonName = await prisonService.getPrisonName(res.locals.user.username, prisonId)
     const prisonInfo = prisons.find(prison => prison.code === prisonId)
 
-    res.render('pages/prisons/edit', { prisonId, prisonInfo, prisonName })
+    res.render('pages/prisons/edit', { prisonId, prisonInfo, prisonName, errors: req.flash('errors'), confirmedChange })
   })
 
   post('/', async (req, res) => {
-    const prisonCode = req.body.prisonId
+    const prisonCode = req.body.prisonId.toUpperCase()
 
     await prisonService.createPrison(prisonCode, res.locals.user.username)
 
@@ -43,7 +51,13 @@ export default function routes({ prisonService }: Services): Router {
 
     await prisonService.activatePrison(prisonCode, res.locals.user.username)
 
-    return res.redirect(`edit`)
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      req.flash('errors', errors.array() as [])
+      return res.redirect(`edit?confirmedChange=false`)
+    }
+
+    return res.redirect(`edit?confirmedChange=true`)
   })
 
   get('/:prisonId/deactivate', async (req, res) => {
@@ -51,7 +65,13 @@ export default function routes({ prisonService }: Services): Router {
 
     await prisonService.deactivatePrison(prisonCode, res.locals.user.username)
 
-    return res.redirect(`edit`)
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      req.flash('errors', errors.array() as [])
+      return res.redirect(`edit?confirmedChange=false`)
+    }
+
+    return res.redirect(`edit?confirmedChange=true`)
   })
   return router
 }
