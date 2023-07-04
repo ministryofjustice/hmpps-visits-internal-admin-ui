0cb65804e642e36704d412f4688fd2c5499758b7
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2023-07-04 15:41:22 +0100

    Linter

--- server/routes/prisons/locationGroups/index.ts
+++ server/routes/prisons/locationGroups/index.ts
@@ -1,5 +1,4 @@
 import { RequestHandler, Router } from 'express'
-import { ValidationChain } from 'express-validator'
 import { Services } from '../../../services'
 import asyncMiddleware from '../../../middleware/asyncMiddleware'
 import LocationGroupsController from './locationGroupsController'
@@ -8,9 +7,6 @@ export default function routes(services: Services): Router {
   const router = Router()
 
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
-  const post = (path: string | string[], handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
-  const postWithValidation = (path: string | string[], validationChain: ValidationChain[], handler: RequestHandler) =>
-    router.post(path, ...validationChain, asyncMiddleware(handler))
 
   const locationGroups = new LocationGroupsController(services.prisonService, services.locationGroupsService)
 
