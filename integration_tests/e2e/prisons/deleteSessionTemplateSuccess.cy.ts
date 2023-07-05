77c9d10297050909137959778f3b5d3176ac05a8
dhiraj.shetty <dhiraj.shetty@digital.justice.gov.uk>
2023-07-05 14:23:15 +0100

    VB-2362 - update integration tests.

--- integration_tests/e2e/prisons/deleteSessionTemplateSuccess.cy.ts
+++ integration_tests/e2e/prisons/deleteSessionTemplateSuccess.cy.ts
@@ -10,7 +10,7 @@ context('Delete a session template success', () => {
 
   beforeEach(() => {
     const activePrison = TestData.prison({ active: true })
-    sessionTemplate = TestData.sessionTemplate({ active: true, prisonId: prisonCode, reference: '-act.dcc.0f' })
+    sessionTemplate = TestData.sessionTemplate({ active: false, prisonId: prisonCode, reference: '-act.dcc.0f' })
     const rangeType: SessionTemplatesRangeType = 'CURRENT_OR_FUTURE'
 
     cy.task('reset')
