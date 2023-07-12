9a9ddd4b31751fa37448c75af2b34a78643aa686
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2023-07-12 08:22:35 +0100

    Comment out post route for PR

--- server/routes/prisons/locationGroups/index.ts
+++ server/routes/prisons/locationGroups/index.ts
@@ -1,5 +1,5 @@
 import { RequestHandler, Router } from 'express'
-import { ValidationChain } from 'express-validator'
+// import { ValidationChain } from 'express-validator'
 import { Services } from '../../../services'
 import asyncMiddleware from '../../../middleware/asyncMiddleware'
 import LocationGroupsController from './locationGroupsController'
@@ -10,8 +10,8 @@ export default function routes(services: Services): Router {
   const router = Router()
 
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
-  const postWithValidation = (path: string | string[], validationChain: ValidationChain[], handler: RequestHandler) =>
-    router.post(path, ...validationChain, asyncMiddleware(handler))
+  // const postWithValidation = (path: string | string[], validationChain: ValidationChain[], handler: RequestHandler) =>
+  //   router.post(path, ...validationChain, asyncMiddleware(handler))
 
   const locationGroups = new LocationGroupsController(services.prisonService, services.locationGroupService)
   const singleLocationGroup = new SingleLocationGroupController(services.prisonService, services.locationGroupService)
@@ -20,7 +20,7 @@ export default function routes(services: Services): Router {
   get('/prisons/:prisonId([A-Z]{3})/location-groups', locationGroups.view())
   get('/prisons/:prisonId([A-Z]{3})/location-groups/add', addLocationGroup.view())
   get('/prisons/:prisonId([A-Z]{3})/location-groups/:reference', singleLocationGroup.view())
-  postWithValidation('/prisons/:prisonId/location-groups/add', addLocationGroup.validate(), addLocationGroup.submit())
+  // postWithValidation('/prisons/:prisonId/location-groups/add', addLocationGroup.validate(), addLocationGroup.submit())
 
   return router
 }
