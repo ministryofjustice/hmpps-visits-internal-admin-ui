5d93d6b9849cfb8601a634e3c4262f9d38027e9f
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-07-19 17:25:33 +0100

    VB-2431 Add a location group (#75)
    * Add location group form
    
    * Render all remove buttons on reload for validation errors
    
    * Remove excessive classes and style with CSS
    
    * Add error messages to level code rows
    
    * Render remove buttons in JavaScript
    
    This prevents them being triggered when pressent enter in a field.
    
    * Add validation for locations not being empty
    
    * Tweak 'Add' buttons
    
    * Tests for Add location group
    
    * Fix validation for adding incentive groups

--- server/routes/prisons/locationGroups/index.ts
+++ server/routes/prisons/locationGroups/index.ts
@@ -1,5 +1,5 @@
 import { RequestHandler, Router } from 'express'
-// import { ValidationChain } from 'express-validator'
+import { ValidationChain } from 'express-validator'
 import { Services } from '../../../services'
 import asyncMiddleware from '../../../middleware/asyncMiddleware'
 import LocationGroupsController from './locationGroupsController'
@@ -11,8 +11,8 @@ export default function routes(services: Services): Router {
 
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
   const post = (path: string | string[], handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
-  // const postWithValidation = (path: string | string[], validationChain: ValidationChain[], handler: RequestHandler) =>
-  // router.post(path, ...validationChain, asyncMiddleware(handler))
+  const postWithValidation = (path: string | string[], validationChain: ValidationChain[], handler: RequestHandler) =>
+    router.post(path, ...validationChain, asyncMiddleware(handler))
 
   const locationGroups = new LocationGroupsController(services.prisonService, services.locationGroupService)
   const singleLocationGroup = new SingleLocationGroupController(services.prisonService, services.locationGroupService)
@@ -22,7 +22,8 @@ export default function routes(services: Services): Router {
   get('/prisons/:prisonId([A-Z]{3})/location-groups/add', addLocationGroup.view())
   get('/prisons/:prisonId([A-Z]{3})/location-groups/:reference', singleLocationGroup.view())
   post('/prisons/:prisonId([A-Z]{3})/location-groups/:reference/delete', singleLocationGroup.delete())
-  // postWithValidation('/prisons/:prisonId/location-groups/add', addLocationGroup.validate(), addLocationGroup.submit())
+
+  postWithValidation('/prisons/:prisonId/location-groups/add', addLocationGroup.validate(), addLocationGroup.add())
 
   return router
 }
