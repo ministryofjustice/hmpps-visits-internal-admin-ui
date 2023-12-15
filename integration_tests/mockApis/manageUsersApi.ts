169904d520a0d9aac1cbf96fca1f5a58528fe579
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2023-12-15 10:53:57 +0000

    Remove deprecated field

--- integration_tests/mockApis/manageUsersApi.ts
+++ integration_tests/mockApis/manageUsersApi.ts
@@ -14,7 +14,6 @@ const stubUser = (name: string) =>
         'Content-Type': 'application/json;charset=UTF-8',
       },
       jsonBody: {
-        staffId: 231232,
         username: 'USER1',
         active: true,
         name,
