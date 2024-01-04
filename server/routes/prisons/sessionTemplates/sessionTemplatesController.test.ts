8e63f373ebe738b52048b214d87d80c45790600d
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-01-04 09:20:11 +0000

    Expand and improve session template tests

--- server/routes/prisons/sessionTemplates/sessionTemplatesController.test.ts
+++ server/routes/prisons/sessionTemplates/sessionTemplatesController.test.ts
@@ -31,15 +31,15 @@ afterEach(() => {
 
 describe('Session templates listing page', () => {
   describe('GET /prisons/{:prisonId}/session-templates', () => {
-    const sessionTemplates = [TestData.sessionTemplate()]
+    const sessionTemplate = TestData.sessionTemplate()
 
     beforeEach(() => {
-      sessionTemplateService.getSessionTemplates.mockResolvedValue(sessionTemplates)
+      sessionTemplateService.getSessionTemplates.mockResolvedValue([sessionTemplate])
     })
 
     it('should display session templates listing page for the prison', () => {
       return request(app)
-        .get('/prisons/HEI/session-templates')
+        .get(`/prisons/${prison.code}/session-templates`)
         .expect('Content-Type', /html/)
         .expect(res => {
           const $ = cheerio.load(res.text)
@@ -53,11 +53,35 @@ describe('Session templates listing page', () => {
 
           expect($('.moj-sub-navigation__item').length).toBe(6)
           expect($('.moj-sub-navigation__link[aria-current]').text()).toBe('Session templates')
-          expect($('.moj-sub-navigation__link[aria-current]').attr('href')).toBe('/prisons/HEI/session-templates')
+          expect($('.moj-sub-navigation__link[aria-current]').attr('href')).toBe(
+            `/prisons/${prison.code}/session-templates`,
+          )
 
           expect($('h2').text().trim()).toContain('Session templates')
+          expect($('[data-test="add-session-template"]').attr('href')).toBe(
+            `/prisons/${prison.code}/session-templates/add`,
+          )
 
-          // TODO - add tests for the session template filter buttons and listing table
+          expect($('h3').eq(0).text().toLocaleUpperCase()).toBe(sessionTemplate.dayOfWeek)
+
+          expect($('[data-test="template-name"]').text()).toBe(sessionTemplate.name)
+          expect($('[data-test="template-name"] a').attr('href')).toBe(
+            `/prisons/${prison.code}/session-templates/${sessionTemplate.reference}`,
+          )
+          expect($('[data-test="template-status"]').text().trim()).toBe('Active')
+          expect($('[data-test="template-start-end-time"]').text().trim()).toBe('13:45 to 14:45')
+          expect($('[data-test="template-capacity-open"]').text().trim()).toBe('35')
+          expect($('[data-test="template-capacity-closed"]').text().trim()).toBe('2')
+          expect($('[data-test="template-valid-from-date"]').text().trim()).toBe('21 Mar 2023')
+          expect($('[data-test="template-valid-to-date"]').text().trim()).toBe('No end date')
+          expect($('[data-test="template-weekly-frequency"]').text().trim()).toBe('1 week')
+          expect($('[data-test="template-groups"]').text().trim()).toBe('None')
+
+          expect(sessionTemplateService.getSessionTemplates).toHaveBeenCalledWith(
+            undefined,
+            prison.code,
+            'CURRENT_OR_FUTURE',
+          )
         })
     })
   })
