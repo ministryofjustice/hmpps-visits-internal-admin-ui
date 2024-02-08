9fb695c1a06296f7592a59bbb886265133c5b093
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-02-08 16:56:45 +0000

    Update dependencies and bring in changes from TypeScript template app (#126)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
    Co-authored-by: Matt <34448412+mattops@users.noreply.github.com>
    Co-authored-by: Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
    Co-authored-by: Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
    Co-authored-by: petergphillips <peter@greenthistle.co.uk>
    Co-authored-by: Andy Marke <andy.marke@digital.justice.gov.uk>
    Co-authored-by: Darren Oakley <darren.oakley@digital.justice.gov.uk>
    Co-authored-by: markreesmoj <76954782+markreesmoj@users.noreply.github.com>
    Co-authored-by: Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
    Co-authored-by: Gareth.m.Davies <gareth.davies@digital.justice.gov.uk>
    Co-authored-by: ushkarev <ushkarev@users.noreply.github.com>
    Co-authored-by: richardpopple <richard.popple@digital.justice.gov.uk>
    Co-authored-by: Michael Willis <michael.willis@digital.justice.gov.uk>
    Co-authored-by: Louise N <louise.norris@digital.justice.gov.uk>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: sp-watson <77974320+sp-watson@users.noreply.github.com>
    Co-authored-by: sp-watson <steve.watson@digital,justice.gov.uk>
    Co-authored-by: Jon Wyatt <jonnywyatt2@gmail.com>
    Co-authored-by: Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
    Co-authored-by: Richard James <44123869+richpjames@users.noreply.github.com>
    Co-authored-by: Stuart Harrison <pezholio@gmail.com>
    Co-authored-by: Neil Mendum <nmendum@gmail.com>
    Co-authored-by: carlov20 <carlov20@users.noreply.github.com>
    Co-authored-by: Neil Mendum <neil.mendum@digital.justice.gov.uk>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: David Middleton <122619525+davidmiddletonmoj@users.noreply.github.com>
    Co-authored-by: Gareth.m.Davies <gareth.m.davies@gmail.com>
    Co-authored-by: bryangaledxw <94137563+bryangaledxw@users.noreply.github.com>
    Co-authored-by: ravmoj <104509282+ravmoj@users.noreply.github.com>
    Co-authored-by: Jon Brighton <brightonsbox@hotmail.com>
    Co-authored-by: Steve Rendell <32732937+steverendell@users.noreply.github.com>
    Co-authored-by: Marcus Aspin <maspin@unilink.com>
    Co-authored-by: Tom Ridd <twridd@gmail.com>
    Co-authored-by: nishanthmahasamudram <129746957+nishanthmahasamudram@users.noreply.github.com>
    Co-authored-by: BarryGeeMOJ <104000682+BarryGeeMOJ@users.noreply.github.com>
    Co-authored-by: Ynda Jas <40244233+yndajas@users.noreply.github.com>

--- server/middleware/setUpStaticResources.ts
+++ server/middleware/setUpStaticResources.ts
@@ -26,10 +26,6 @@ export default function setUpStaticResources(): Router {
     router.use('/assets', express.static(path.join(process.cwd(), dir), cacheControl))
   })
 
-  Array.of('/node_modules/govuk_frontend_toolkit/images').forEach(dir => {
-    router.use('/assets/images/icons', express.static(path.join(process.cwd(), dir), cacheControl))
-  })
-
   Array.of('/node_modules/jquery/dist/jquery.min.js').forEach(dir => {
     router.use('/assets/js/jquery.min.js', express.static(path.join(process.cwd(), dir), cacheControl))
   })
