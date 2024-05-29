99f75ccf39570c09c4d660dab8bab8fa47ccd6eb
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2024-05-29 11:07:36 +0100

    Set up form, routes and validation

--- server/routes/bookers/bookersController.ts
+++ server/routes/bookers/bookersController.ts
@@ -1,11 +1,71 @@
 import { RequestHandler } from 'express'
+import { ValidationChain, body, validationResult } from 'express-validator'
+import { responseErrorToFlashMessage } from '../../utils/utils'
 
 export default class BookersController {
   public constructor() {}
 
   public view(): RequestHandler {
     return async (req, res) => {
-      return res.render('pages/bookers/index')
+      return res.render('pages/bookers/index', {
+        errors: req.flash('errors'),
+        message: req.flash('message'),
+      })
     }
+  }
+
+  public updateBookerDetails(): RequestHandler {
+    return async (req, res) => {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        req.flash('errors', errors.array())
+        req.flash('formValues', req.body)
+        return res.redirect('/bookers')
+      }
+
+      const { bookerEmail, prisonerId, visitorId } = req.body
+      // visitorIds.split(/\s+/)
+
+      try {
+        // TODO call API
+        req.flash('message', `UPDATED!!! `)
+      } catch (error) {
+        req.flash('errors', responseErrorToFlashMessage(error))
+      }
+      return res.redirect('/bookers')
+    }
+  }
+
+  public clearBookerDetails(): RequestHandler {
+    return async (req, res) => {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        req.flash('errors', errors.array())
+        req.flash('formValues', req.body)
+        return res.redirect('/bookers')
+      }
+
+      const { bookerReference } = req.body
+
+      try {
+        // TODO call API
+        req.flash('message', `Details cleared for booker '${bookerReference}'`)
+      } catch (error) {
+        req.flash('errors', responseErrorToFlashMessage(error))
+      }
+      return res.redirect('/bookers')
+    }
+  }
+
+  public validateClearBookerDetails(): ValidationChain[] {
+    return [body('bookerReference', 'Invalid booker reference').isLength({ min: 5, max: 30 })]
+  }
+
+  public validateUpdateBookerDetails(): ValidationChain[] {
+    return [
+      body('bookerEmail', 'Invalid booker email').isEmail(),
+      body('prisonerId', 'Invalid prisoner number').isLength({ min: 5, max: 10 }),
+      body('visitorIds', 'Invalid visitor IDs').trim().whitelist('\\d').isLength({ min: 4, max: 100 }),
+    ]
   }
 }
--- server/routes/bookers/index.ts
+++ server/routes/bookers/index.ts
@@ -1,4 +1,5 @@
 import { RequestHandler, Router } from 'express'
+import { ValidationChain } from 'express-validator'
 import { Services } from '../../services'
 import BookersController from './bookersController'
 import asyncMiddleware from '../../middleware/asyncMiddleware'
@@ -8,10 +9,15 @@ export default function routes(services: Services): Router {
 
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
   const post = (path: string | string[], handler: RequestHandler) => router.post(path, asyncMiddleware(handler))
+  const postWithValidation = (path: string | string[], validationChain: ValidationChain[], handler: RequestHandler) =>
+    router.post(path, ...validationChain, asyncMiddleware(handler))
 
   const bookers = new BookersController()
 
   get('/bookers', bookers.view())
+
+  postWithValidation('/bookers/booker/update', bookers.validateUpdateBookerDetails(), bookers.updateBookerDetails())
+  postWithValidation('/bookers/booker/clear', bookers.validateClearBookerDetails(), bookers.clearBookerDetails())
 
   return router
 }
