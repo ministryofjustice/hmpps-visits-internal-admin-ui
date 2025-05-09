5f020e3ba6da570fb2bc958030bb1eacd51d5bab
hutcheonb <106151419+hutcheonb-moj@users.noreply.github.com>
2025-05-09 18:13:39 +0100

    missed integration test

--- integration_tests/e2e/prisons/sessionTemplates/viewSingleSessionTemplate.cy.ts
+++ integration_tests/e2e/prisons/sessionTemplates/viewSingleSessionTemplate.cy.ts
@@ -54,6 +54,6 @@ context('Session templates - single', () => {
     viewSingleSessionTemplatePage.getCategoryGroups().contains('None')
     viewSingleSessionTemplatePage.getIncentiveGroups().contains('None')
     viewSingleSessionTemplatePage.getLocationGroups().contains('None')
-    viewSingleSessionTemplatePage.getPublicVisbility().contains('Yes')
+    viewSingleSessionTemplatePage.getPublicVisbility().contains('No')
   })
 })
