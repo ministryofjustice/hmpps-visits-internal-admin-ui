dc721ac1fef1f70ffed91a5c718cbbfe1e20fda4
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2025-10-21 15:39:34 +0100

    Fix int test

--- integration_tests/e2e/signIn.cy.ts
+++ integration_tests/e2e/signIn.cy.ts
@@ -30,7 +30,7 @@ context('Sign In', () => {
   it('Phase banner visible in header', () => {
     cy.signIn()
     const indexPage = Page.verifyOnPage(IndexPage)
-    indexPage.headerPhaseBanner().should('contain.text', 'dev')
+    indexPage.headerPhaseBanner().should('contain.text', 'DEV')
   })
 
   it('User can sign out', () => {
