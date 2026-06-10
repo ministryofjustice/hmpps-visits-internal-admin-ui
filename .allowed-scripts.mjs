6f1a6b80cbe027a0122122b8228a1de389b5d34a
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2026-06-10 09:39:22 +0100

    Update dependencies 2026-06-10 (#423)

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -5,12 +5,12 @@ export default configureAllowedScripts({
     // Needed by esbuild for watching files during development
     'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
     // Cypress
-    'node_modules/cypress@^15.13.1': 'ALLOW',
+    'node_modules/cypress@^15.16.0': 'ALLOW',
     // Provides native integration, supporting ability to write dtrace probes for bunyan
-    'node_modules/dtrace-provider@0.8.8': 'ALLOW',
+    'node_modules/dtrace-provider@^0.8.8': 'FORBID',
     // Needed by jest for running tests in watch mode
-    'node_modules/fsevents@2.3.3': 'ALLOW',
+    'node_modules/fsevents@^2.3.3': 'FORBID',
     // Native solution to quickly resolve module paths, used by jest and eslint
-    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
+    'node_modules/unrs-resolver@1.12.2': 'ALLOW',
   },
 })
